0c3233dbe77a8f323fe1619d563818dd74e2a7c2
micscoob <33203811+micscoob@users.noreply.github.com>
2017-10-30 12:44:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ def vowel_check(string):
 
     return x
 
+def constent(string):
+    contents = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
+    x = 0
+    for i in string:
+        if i in contents:
+            x += 1    
+    print('Here are the constinets %d' % x)
+string = input('Type a string in:\n')
+constent(string)
+vowel_check(string)
+
+
 if __name__ == '__main__':
     text = input('Enter your sentence\n')
