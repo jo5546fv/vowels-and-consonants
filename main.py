5fc55da8f196fa271f5fb454a811ed4d582f5a24
jo5546fv <33111069+jo5546fv@users.noreply.github.com>
2017-10-30 13:12:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def vowel_check(string):
+    '''check for vowels'''
     vowels = ['a','e','i','o','u']
     x = 0
-    
     for i in string:
         if i in vowels:
             x += 1
     return x
     
 def consonant(string):
+    '''check for consonants'''
     consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
     x = 0
     for i in string:
@@ -15,8 +16,6 @@ def consonant(string):
             x += 1    
     return x
     
-
-
 if __name__ == '__main__':
     text = input('Type a string in:\n')
     
