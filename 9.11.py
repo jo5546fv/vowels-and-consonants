1a4f9cbf0cb3904e90263a999372b47409aaa5ef
micscoob <33203811+micscoob@users.noreply.github.com>
2017-11-14 15:58:35 -0600

    Update 9.11.py

--- 9.11.py
+++ 9.11.py
@@ -1,3 +1,11 @@
+def print_menu():       
+    print  ("MENU")
+    print("a - Add item to cart")
+    print("r - Remove item from cart")
+    print("c - Change item quantity")
+    print("i - Output items' descriptions")
+    print("o - Output shopping cart")
+    print('q - Quit')
 class ItemToPurchase:
     
     def __init__(self):
@@ -15,8 +23,8 @@ class ItemToPurchase:
 
 class ShoppingCart:
     def __init__(self, name, date):
-        self.customer_name = 'none
-        self.current_date = 'January 1, 2016'
+        self.name = name
+        self.date = date
         cart_items = []
 
     def add_item(self):
@@ -30,21 +38,25 @@ class ShoppingCart:
 
     def modify_item(self):
         if self.item_name in cart_items:
-            #TODO:
+            pass#TODO:
 
     def get_num_items_in_cart(self):
         return len(cart_items + 1)
 
     def get_cost_of_cart(self):
-        #TODO:
+       pass #TODO:
 
     def print_total(self):
-        #TODO:
+        pass#TODO:
 
     def print_descriptions(self):
-        #TODO:
+        pass#TODO:
+    
 
 if __name__ == "__main__":
+    name = input("Enter customer's name:\n")
+    date = input("Enter today's date:\n")
+
     print('Item 1')
     item1 = ItemToPurchase()
     item1.item_name = input('Enter the item name:\n')
@@ -63,3 +75,33 @@ if __name__ == "__main__":
     item2.print_item_cost()
     
     print('\nTotal: $%d' % ((item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)))
+
+    ShoppingCart(name, date)
+   
+#PRINTS MENU#
+while True:          
+    print_menu()    
+    choice = input("Choose an option: ")
+     
+    if choice=='a':     
+        print("a - Add item to cart")
+        
+    elif choice=='r':
+        print("r - Remove item from cart")
+        
+    elif choice=='c':
+        print("c - Change item quantity")
+       
+    elif choice=='i':
+        print("Output items' descriptions")
+       
+    elif choice=='o':
+        print("Output shopping cart")
+        
+    elif choice=='q':
+        break
+    else:
+        
+        input("Wrong option selection. Enter any key to try again..")
+
+  
