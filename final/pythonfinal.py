35a4bb1f3f04e70fb04614b31ffbe372c146ef5c
jo5546fv <33111069+jo5546fv@users.noreply.github.com>
2017-12-07 23:29:31 -0600

    Update pythonfinal.py

--- final/pythonfinal.py
+++ final/pythonfinal.py
@@ -1,6 +1,99 @@
+from tkinter import *
 import csv
 
-teams = {}
+'''class fantasy_teams:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Enter teams")
+
+        self.team = '\n'
+        self.entered_team = ''
+        
+        self.team_txt = StringVar()
+        self.team_txt.set(self.team)
+        self.team_label = Label(master, textvariable=self.team_txt)
+        
+        self.label = Label(master, text='Teams:')
+
+        vcmd = master.register(self.validate)
+        self.entry = Entry(master, validate='key', validatecommand=(vcmd, '%P'))
+
+        self.team_button = Button(master, text='ENTER', command=lambda: self.update())
+        self.done_button = Button(master, text='DONE', command=self.master.destroy)
+        
+        self.label.grid(row=0, column=0, sticky=W)
+        self.team_label.grid(row=0, column=1, columnspan=2, sticky=E)
+
+        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+        self.team_button.grid(row=2, column=0)
+        self.done_button.grid(row=2, column=1)
+
+    def validate(self, new_text):
+        teams = {}
+        if not new_text:
+            self.entered_team = ''
+            return True
+        self.entered_team = new_text
+        teams[new_text] = []
+        print(teams)
+        return teams
+    def update(self):
+        self.team = self.entered_team
+
+        self.team_txt.set(self.team)
+        self.entry.delete(0, END)
+
+root = Tk()
+my_qui = fantasy_teams(root)
+root.mainloop()
+'''
+
+
+
+
+class makin_teams:
+    def __init__(self, master):
+        self.teams = {}
+        self.person = {}
+        
+        self.master = master
+        self.label_1 = Label(master, text='Name')
+        self.label_2 = Label(master, text='Team')
+
+        self.team_name = StringVar()
+        self.name_name = StringVar()
+        
+        self.entry_1 = Entry(master, textvariable=self.name_name)
+        self.entry_2 = Entry(master, textvariable=self.team_name)
+
+        self.button_1 = Button(
+            master, text='ENTER', command=lambda :self.add_team(self.team_name.get(), self.name_name.get()))
+        self.button_2 = Button(master, text='DONE', command=self.master.destroy)
+
+        self.label_1.grid(row=0, sticky=E)
+        self.label_2.grid(row=1, sticky=E)
+
+        self.button_1.grid(row=2)
+        self.button_2.grid(row=2, column=1)
+
+        self.entry_1.grid(row=0, column=1)
+        self.entry_2.grid(row=1, column=1)
+
+    def add_team(self, team, name):
+        self.person[name] = self.teams[team]
+        print(self.person)
+
+
+root = Tk()
+b = makin_teams(root)
+root.mainloop()
+
+
+
+
+#basics start here
 while True:
     team_name = input('enter team name - q to quit.\n')
     if team_name == 'q':
