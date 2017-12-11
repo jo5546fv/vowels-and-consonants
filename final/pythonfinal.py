425cb65b99b566b626baca9e065d9ee93d47fb30
jo5546fv <33111069+jo5546fv@users.noreply.github.com>
2017-12-11 16:05:39 -0600

    Update pythonfinal.py

--- final/pythonfinal.py
+++ final/pythonfinal.py
@@ -2,7 +2,7 @@ from tkinter import *
 import csv
 from functools import partial
 
-#add teams gui
+#all the teams will be added through this gui
 class makin_teams:
     def __init__(self, master):
         self.teams = {}
@@ -55,8 +55,8 @@ class makin_teams:
         self.master.destroy()
 
 
-#set teams in desired draft order
-class draft_orderin:
+
+class draft_orderin: #set teams in desired draft order
     def __init__(self, master):
         self.order = []
         self.master = master
@@ -71,12 +71,12 @@ class draft_orderin:
         self.done_button = Button(master, text='DONE', command=self.master.destroy)
         self.done_button.pack()
 
-    def store(self, i, team):
+    def store(self, i, team): #destroy the button when its pressed
         self.order.append(team)
         self.bname = (self.button_identities[i])
         self.bname.destroy()
 
-    def order_return(self):
+    def order_return(self): #return the list containing the team order
         return self.order
 
 
@@ -89,28 +89,21 @@ def playercsv():
             player[row[1]+' '+row[3] +' '+row[2]] = [row[2], row[3]]
         return player
 
-times = 150
-class the_draft:
+
+class the_draft: #this is where all the drafting of players happens
     def __init__(self, master):
         self.master = master
         self.master.focus()
         self.turn = 0
         self.player_search = ''
-#       self.timer = 0
-
-#        self.timer_label = Label(master, text='', width=10)
-#        self.timer_label.grid(row=0, column=7, sticky=E)
+        
         self.listbox = Listbox(master)
         self.listbox.bind('<Double-Button-1>', self.entry_insert)
         self.listbox.bind('<Return>', self.entry_insert)
-        for player in players:
+        
+        for player in players: #insert every player into the listbox
             self.listbox.insert(END, player)
         self.listbox.grid(row=3)
-
-##        self.draft_table_var = StringVar()
-##        self.draft_table_var.set(self.table())
-##        self.draft_table = Label(master, textvariable=self.draft_table_var)
-##        self.draft_table.grid(row=3, column=5, stick=W)
         
         self.player_label_var = StringVar()
         self.player_label_var.set(self.drafted())
@@ -131,135 +124,122 @@ class the_draft:
         self.player = StringVar()
         self.player_entry = Entry(master, textvariable=self.player)
         self.player_entry.grid(row=2)
-        self.player_entry.bind('<Tab>', self.do_nothing)
-        self.player_entry.bind('<BackSpace>', self.edit_search)
-        self.player_entry.bind('<Return>',self.add_player)
-        self.player_entry.bind('<Key>', self.search_players)
+        
+        self.player_entry.bind('<Tab>', self.do_nothing) #literally
+        self.player_entry.bind('<BackSpace>', self.edit_search) #instead of adding backspace to the search actually backspace
+        self.player_entry.bind('<Return>',self.add_player) #draft the player
+        self.player_entry.bind('<Key>', self.search_players) #bind all other keys to search
         self.player_entry.focus()
-#        self.countdown(150)
+        
 
-#    def countdown(self, time=times):
-#        if time >= 0:
-#            self.m = time / 60
-#            self.s = time % 60
-#            self.timer_label.configure(text='%d:%d' % (self.m, self.s))
-#            times = time - 1
-#            root.after(1000, self.countdown)
-#        else:
-#            self.timer_label.configure(text='time\'s up!')
-
-
-    def do_nothing(self, master):
-        #prevents a tab search error by making tab call to this instead of <Key>
+    def do_nothing(self, master): #prevents a tab search error by making tab call to this instead of <Key>
         pass
 
-    def search_players(self, master=0):
+    def search_players(self, master=0): #searches the csv and edits the listbox with relevent players
         self.listbox.delete(0, END)
-        try:
+        
+        try: #dont add a character to the search string if its not space or a letter
             if master.char.isalpha() == True or master.char.isspace() == True:
                 self.player_search += master.char.lower()
         except:
             pass
-        for player in players:
+        
+        for player in players: #search the list with supplied string and add the players to the listbox
             if self.player_search in player.lower():
                 self.listbox.insert(END, player)
 
-    def edit_search(self, master):
+    def edit_search(self, master): #backspace calls this, deletes last character then searches for players
         self.player_search = self.player_search[:-1]
         self.search_players()
 
-    def entry_insert(self, master):
+    def entry_insert(self, master): # removes any characters from the entry box then adds the selected player to entry box
         self.player_entry.delete(0, END)
         self.player_entry.insert(0, self.listbox.get(self.listbox.curselection()))
         self.player_entry.focus()
 
-    def add_player(self, master):
-        if self.player.get() != '':
+    def add_player(self, master): # stores the player under the relevent team in the teams dictionary
+        if self.player.get() != '': # prevents accidentally drafting nobody
             teams[draft_order[self.turn]].append(self.player.get())
-            try:
+            try: # delete the player from the players dictionary, if player exists. This allows adding a player thats not listed
                 del players[self.player.get()]
             except:
                 pass
-            self.player_entry.delete(0, END)
+            
+            self.player_entry.delete(0, END) #cleanup
             self.turn += 1
             self.listbox.delete(0, END)
             self.player_search = ''
-            for player in players:
+            
+            for player in players: #add all the players back into the listbox
                 self.listbox.insert(END, player)
-            if self.turn >= len(draft_order):
+            
+            if self.turn >= len(draft_order): #snake draft so 1,2,3,3,2,1,1,2,3,...
                 self.turn = 0
                 draft_order.reverse()
 
-        self.team_label_var.set(draft_order[self.turn])
-        self.player_label_var.set(self.drafted())
-        self.draft_table_var.set(self.table())
+        self.team_label_var.set(draft_order[self.turn]) #change label to active team
+        self.player_label_var.set(self.drafted()) #show your drafted players
 
-    def drafted(self):
+    def drafted(self): #show all your drafted players
         self.team_players = ''
-        for player in teams[draft_order[self.turn]]:
+        
+        for player in teams[draft_order[self.turn]]: #add all your players to a string that will be in the label
             self.team_players += player + '\n'
+        
         return self.team_players
 
-##    def table(self):
-##        self.team_table = []
-##        self.x = 0
-##        self.y = 0
-##        self.player_table = ''
-##        for team in teams:
-##            self.team_table.append(team)
-##            self.player_table += '|%30s|' % team
-##        self.player_table += '\n'
-##        while self.x < len(teams[self.team_table[self.x]]):
-##            self.all_players = []
-##            while self.y < len(teams):
-##                self.player_table +='|%30s|' % teams[self.team_table[self.y]][self.x]
-##                self.y += 1
-##            self.player_table += '\n'
-##            self.x += 1
-##            self.y = 0
-##        return self.player_table
-  
-root = Tk()
-teamers = makin_teams(root)
-root.mainloop()
 
-#store team dictionary: player, and owner:teamname
-teams = teamers.add_team()[0]
-owners = teamers.add_team()[1]
-try:
-    del teams['']
-    del owners['']
-except:
-    pass
-
-master = Tk()
-orderin = draft_orderin(master)
-master.mainloop()
-draft_order = orderin.order_return()
-
-players = playercsv()
-
-root = Tk()
-draftin = the_draft(root)
-root.mainloop()
-
-with open('2017draft.csv', 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    all_teams = []
-    all_owners = []
-    y = 0
-    x = 0
-    for team in teams:
-        all_owners.append(owners[team])
-    csv_writer.writerow(all_owners)
-    for team in teams:
-        all_teams.append(team)
-    csv_writer.writerow(teams)
-    while y < len(teams[all_teams[x]]):
-        all_players = []
-        while x < len(teams):
-            all_players.append(teams[all_teams[x]][y])
-            x += 1
-        csv_writer.writerow(all_players)
-        y += 1
+def draft_write():    
+    with open('2017draft.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        all_teams = []
+        all_owners = []
+        y = 0
         x = 0
+        for team in teams:
+            all_owners.append(owners[team])
+        csv_writer.writerow(all_owners)
+        for team in teams:
+            all_teams.append(team)
+        csv_writer.writerow(teams)
+        while y < len(teams[all_teams[x]]):
+            all_players = []
+            while x < len(teams):
+                all_players.append(teams[all_teams[x]][y])
+                x += 1
+            csv_writer.writerow(all_players)
+            y += 1
+            x = 0
+
+if __name__ == "__main__":
+            
+    #run the makin_teams class
+    root = Tk()
+    teamers = makin_teams(root)
+    root.mainloop()
+
+    #store team dictionary: player, and owner:teamname
+    teams = teamers.add_team()[0]
+    owners = teamers.add_team()[1]
+    try:
+        del teams['']
+        del owners['']
+    except:
+        pass
+
+    #run the draft_orderin class
+    master = Tk()
+    orderin = draft_orderin(master)
+    master.mainloop()
+    draft_order = orderin.order_return()
+
+    #import all players into dictionary
+    players = playercsv()
+
+    #run the_draft class
+    root = Tk()
+    draftin = the_draft(root)
+    root.mainloop()
+
+    draft_write() #store draft in csv
+
